1d427cc47fd1271c45fe085bb51c3b7eb77f3112
Orajiaku Onyinyechi <onyiorajiaku1@gmail.com>
2022-06-07 21:04:55 +0100

    Solve Python Task

--- src/app.py
+++ src/app.py
@@ -1,12 +1,32 @@
-import sys
 import os
+import sys
+import math
 
 
-def prime(s):
+number = int(input())
+
+
+def prime(num):
     # your code goes here
+    # function to determine prime number
+    for i in range(2, int(math.sqrt(num) + 1)):
+        if num % i == 0:
+            return False
+    return True
 
-def solution(s):
-    return prime(s)
+
+# prints whether an input is a prime number or not
+for j in range(number):
+    n = int(input())
+
+    if n >= 2 and prime(n):
+        print('The number you entered is a Prime number.')
+    else:
+        print('The number you entered is not a Prime number')
+
+
+def solution(num):
+    return prime(num)
 
 
 if __name__ == "__main__":
